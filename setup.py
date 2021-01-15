a573ebdaa0cd059c72ea6f679f7eddabc2645f4e
Alon Moses <alon2332@gmail.com>
2021-01-15 19:06:52 +0200

    Rearanging Directory

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='M',
+version='0.1',
+description='Installation of Package',
+url='#',
+author='alon moses',
+author_email='alon2332@gmail.com',
+packages=['M'],
+zip_safe=False)
